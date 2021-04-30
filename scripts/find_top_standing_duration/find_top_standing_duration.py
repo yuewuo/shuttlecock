b46154f86ad879435552854f8c7bd5545b2d7d4e
Yue <yue.wu@yale.edu>
2021-04-30 21:34:51 +0800

    add find top standing duration script

---
+++ scripts/find_top_standing_duration/find_top_standing_duration.py
@@ -0,0 +1,58 @@
+import os, re
+
+class PersonBest:
+    def __init__(self, classname, name):
+        self.classname = classname
+        self.name = name
+        self.best_duration = 0
+        self.best_date = None
+
+def main():
+    classes = ["class1", "class2", "class3", "class4", "class5", "class6", "grad"]
+    best_persons = []
+    for classname in classes:
+        table = buildtable(classname)
+        for row in table[1:]:
+            if len(row) < 1:
+                continue
+            name = row[0]
+            person_best = PersonBest(classname, name)
+            for i in range(1, len(row)):
+                val = row[i]
+                if val == "":
+                    continue
+                all_val = re.findall(r"[0-9]+", val)
+                assert(len(all_val) == 3)
+                val0 = int(all_val[0])
+                val1 = int(all_val[1])
+                if val0 > person_best.best_duration:
+                    person_best.best_duration = val0
+                    person_best.best_date = table[0][i]
+                if val1 > person_best.best_duration:
+                    person_best.best_duration = val1
+                    person_best.best_date = table[0][i]
+            # print(classname, name, person_best.best_duration, person_best.best_date)
+            best_persons.append(person_best)
+        # exit(0)
+    best_persons.sort(key = lambda x: -x.best_duration)
+    for i in range(10):
+        person_best = best_persons[i]
+        print(person_best.classname, person_best.name, person_best.best_duration, person_best.best_date)
+
+def buildtable(classname, csvname = None):
+    csvcontent = ""
+    if csvname is None:
+        csvname = "%s.csv" % classname
+    with open(csvname, "r", encoding="gb18030") as f:
+        csvcontent = f.read()
+    # print(csvcontent)
+    csvsp1 = csvcontent.split('\n')
+    table = []
+    for ele in csvsp1:
+        sp2 = ele.split(',')
+        if len(sp2) > 1:
+            table.append(sp2)
+    return table
+
+if __name__ == "__main__":
+    main()
